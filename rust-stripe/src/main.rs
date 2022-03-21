60c76079a9e125d8a6085344cc5d55d47f34c4d8
Cristiano Piemontese <cristiano.piemontese@prima.it>
2022-03-21 16:40:47 +0100

    formatting

--- rust-stripe/src/main.rs
+++ rust-stripe/src/main.rs
@@ -14,41 +14,57 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let client = reqwest::Client::new();
     create_a_customer(&client, &stripe_url, &secret_key).await;
-    let response = create_a_payment_intent(client, stripe_url, secret_key, "cus_LMUT6c5j1u8ubB").await;
+    let response =
+        create_a_payment_intent(client, stripe_url, secret_key, "cus_LMUT6c5j1u8ubB").await;
     dbg!(response.text().await);
     Ok(())
 }
 
-async fn create_a_customer(client: &reqwest::Client, stripe_url: &Url, secret_key: &str) -> reqwest::Response {
+async fn create_a_customer(
+    client: &reqwest::Client,
+    stripe_url: &Url,
+    secret_key: &str,
+) -> reqwest::Response {
     let form_data = HashMap::from([
         ("description".to_string(), "A customer".to_string()),
-        ("name".to_string(), "Arturo".to_string())
-        ]);
+        ("name".to_string(), "Arturo".to_string()),
+    ]);
     let resp = client
         .post(stripe_url.join("customers").unwrap())
-        .form(&form_data) 
+        .form(&form_data)
         .basic_auth::<&str, String>(secret_key, None)
         .send()
-        .await.unwrap();
+        .await
+        .unwrap();
 
-
-        resp
+    resp
 }
-async fn create_a_payment_intent(client: reqwest::Client, stripe_url: Url, secret_key: String, customer_id: &str) -> reqwest::Response {
+async fn create_a_payment_intent(
+    client: reqwest::Client,
+    stripe_url: Url,
+    secret_key: String,
+    customer_id: &str,
+) -> reqwest::Response {
     let form_data = HashMap::from([
         ("customer".to_string(), customer_id.to_string()),
         ("currency".to_string(), "eur".to_string()),
         ("amount".to_string(), "1000".to_string()),
-        ("payment_method_types[]".to_string(), "sepa_debit".to_string()),
-        ("metadata[integration_checker]".to_string(), "sepa_debit_accept_a_payment".to_string()),
-        ("setup_future_usage".to_string(), "off_session".to_string())
-        ]);
+        (
+            "payment_method_types[]".to_string(),
+            "sepa_debit".to_string(),
+        ),
+        (
+            "metadata[integration_checker]".to_string(),
+            "sepa_debit_accept_a_payment".to_string(),
+        ),
+        ("setup_future_usage".to_string(), "off_session".to_string()),
+    ]);
     let resp = client
         .post(stripe_url.join("payment_intents").unwrap())
-        .form(&form_data) 
+        .form(&form_data)
         .basic_auth::<String, String>(secret_key, None)
         .send()
         .await;
-    
+
     resp.unwrap()
 }
