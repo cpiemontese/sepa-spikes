8ab44cdc44f75c9277f7f29093ae7403d69005fa
Cristiano Piemontese <cristiano.piemontese@prima.it>
2022-03-22 14:25:11 +0100

    customer form

---
+++ rust-stripe/public/src/customer.js
@@ -0,0 +1,32 @@
+(() => {
+  const form = document.getElementById('customer-form');
+  const customerName = document.getElementById('customer-name');
+  const customerEmail = document.getElementById('customer-email');
+
+  async function postData (url = '', data = {}) {
+    const response = await fetch(url, {
+      method: 'POST',
+      mode: 'cors',
+      cache: 'no-cache',
+      // include, *same-origin, omit
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer',
+      body: JSON.stringify(data)
+    });
+    return response.json();
+  }
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    postData(`${serverUrl}/customer`, { name: customerName.value, email: customerEmail.value })
+      .then(data => {
+        console.log(data); // JSON data parsed by `data.json()` call
+      });
+  });
+})();
+
