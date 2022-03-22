b0c2d69d31440a320392e79a26d25a85b39bddd3
Cristiano Piemontese <cristiano.piemontese@prima.it>
2022-03-22 14:55:29 +0100

    get data from FE

--- rust-stripe/src/routes.rs
+++ rust-stripe/src/routes.rs
@@ -1,33 +1,57 @@
 use std::collections::HashMap;
 
-use actix_web::{get, post, HttpResponse, Responder};
+use actix_web::{get, post, web::Json, HttpResponse, Responder};
 use reqwest::Url;
+use serde::{Deserialize, Serialize};
 
 #[get("/")]
 pub async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
+#[derive(Deserialize)]
+pub struct Customer {
+    pub name: String,
+    pub email: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct StripeCustomer {
+    id: String,
+}
+
 #[post("/customer")]
-pub async fn customer(req_body: String) -> impl Responder {
+pub async fn customer(customer: Json<Customer>) -> HttpResponse {
     let client = reqwest::Client::new();
     let secret_key = std::env::var("STRIPE_SECRET_KEY").expect("Missing STRIPE_SECRET_KEY in env");
     let stripe_url = Url::parse("https://api.stripe.com/v1/").unwrap();
-    create_a_customer(&client, &stripe_url, &secret_key).await;
+    let stripe_customer: StripeCustomer = create_a_customer(
+        &client,
+        &stripe_url,
+        &secret_key,
+        &customer.name,
+        &customer.email,
+    )
+    .await
+    .json()
+    .await
+    .unwrap();
     // let response = create_a_payment_intent(client, stripe_url, secret_key, "cus_LMUT6c5j1u8ubB").await;
     // dbg!(response.text().await);
 
-    HttpResponse::Ok().body(req_body)
+    HttpResponse::Ok().json(stripe_customer)
 }
 
 async fn create_a_customer(
     client: &reqwest::Client,
     stripe_url: &Url,
     secret_key: &str,
+    customer_name: &str,
+    customer_email: &str,
 ) -> reqwest::Response {
     let form_data = HashMap::from([
-        ("description".to_string(), "A customer".to_string()),
-        ("name".to_string(), "Arturo".to_string()),
+        ("name".to_string(), customer_name),
+        ("email".to_string(), customer_email),
     ]);
     let resp = client
         .post(stripe_url.join("customers").unwrap())
