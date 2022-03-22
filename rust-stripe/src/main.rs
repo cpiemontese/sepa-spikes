9a1c43c3a282af97d0236249b5638875f0654625
Cristiano Piemontese <cristiano.piemontese@prima.it>
2022-03-22 14:05:00 +0100

    Revert "let's try sauron! 👁️"
    This reverts commit 9b1f5ecffa2d65017b577edf435dc48b9fa52a0f.

--- rust-stripe/src/main.rs
+++ rust-stripe/src/main.rs
@@ -1,21 +1,12 @@
 extern crate dotenv;
 
-mod app;
-use app::App;
-
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::Arc};
 
 use dotenv::dotenv;
 use reqwest::Url;
-use sauron::{prelude::wasm_bindgen, Program};
 
 #[tokio::main]
-#[wasm_bindgen(start)]
-pub async fn main() {
-    Program::mount_to_body(App::new());
-}
-
-async fn do_stuff() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
 
     let secret_key = std::env::var("STRIPE_SECRET_KEY").expect("Missing STRIPE_SECRET_KEY in env");
@@ -24,7 +15,7 @@ async fn do_stuff() -> Result<(), Box<dyn std::error::Error>> {
     let client = reqwest::Client::new();
     create_a_customer(&client, &stripe_url, &secret_key).await;
     let response =
-        create_a_payment_intent(client, stripe_url, secret_key, "cus_LMWNfdVA38M25w").await;
+        create_a_payment_intent(client, stripe_url, secret_key, "cus_LMUT6c5j1u8ubB").await;
     dbg!(response.text().await);
     Ok(())
 }
