4eaa8e05d9a0a7f0122d438676a3ccef4d1ae358
Cristiano Piemontese <cristiano.piemontese@prima.it>
2022-03-24 10:27:54 +0100

    add create product page

--- rust-stripe/public/src/products.js
+++ rust-stripe/public/src/products.js
@@ -3,9 +3,6 @@
   const productName = document.getElementById('product-name');
   const productPrice = document.getElementById('product-price');
 
-  const priceId = document.getElementById('price-id');
-  const setupIntentForm = document.getElementById('setup-intent-form');
-
   function sanitizePrice (price) {
     return parseFloat(price).toFixed(2)
   }
@@ -13,20 +10,15 @@
   form.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    console.log({ payload: { name: productName.value, price: sanitizePrice(productPrice.value) } });
-
     post(`${SERVER_URL}/products`, { name: productName.value, price: sanitizePrice(productPrice.value) })
       .then(data => {
         console.log({ product: data });
-        form.classList.add('hidden');
-        priceId.value = data.id;
-        setupIntentForm.classList.remove('hidden');
+        Flash.success('Product created successfully!');
       })
       .catch((error) => {
         console.error({ product: error });
         Flash.failure('Something went wrong while creating the product');
       });
-    ;
   });
 })();
 
